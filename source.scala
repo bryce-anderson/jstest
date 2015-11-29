03c8e2f1cccb2c2325903b1feed8a17c7bd0225a
Bryce Anderson <bryce.anderson22@gmail.com>
2015-11-28 21:52:17 -0500

    Add name

--- source.scala
+++ source.scala
@@ -3,7 +3,10 @@ object Main {
 
   /// src_inlined main
   // Added a doc comment here
-  case class Foo(name: String)
+  case class Foo(name: String) {
+    override def toString = name
+  }
+  
   /// end_src_inlined
   
   // end of object
